c044aa4e564af392e7fbb1fcbb93cbdb3ad36849
shenbengit <714081644@qq.com>
2018-03-02 11:47:52 +0800

    查看图片工具类完善，添加保存图片的功能

--- app/src/main/java/com/example/ben/rainy_night/view/EnlargePictureDialog.java
+++ app/src/main/java/com/example/ben/rainy_night/view/EnlargePictureDialog.java
@@ -3,11 +3,14 @@ package com.example.ben.rainy_night.view;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,12 +20,18 @@ import android.widget.TextView;
 
 import com.example.ben.rainy_night.GlideApp;
 import com.example.ben.rainy_night.R;
-import com.luck.picture.lib.entity.LocalMedia;
+import com.example.ben.rainy_night.util.LoggerUtil;
+import com.example.ben.rainy_night.util.ToastUtil;
 import com.luck.picture.lib.widget.PreviewViewPager;
 import com.vondear.rxtools.view.dialog.RxDialog;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import cn.bmob.v3.datatype.BmobFile;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.DownloadFileListener;
 
 /**
  * 重写RxDialog,用于全屏显示一张图片的Dialog
@@ -33,17 +42,21 @@ import java.util.List;
 
 public class EnlargePictureDialog extends RxDialog {
 
-    private List<LocalMedia> images = new ArrayList();
-    private PreviewViewPager mViewPage;
+    private PreviewViewPager mViewPager;
     private ImageView mImage;
     private TextView mTitle;
     private ImageButton mDelete;
-    private int position;
 
-    private OnDeletePictureOnClickListener mListener;
+    private OnDeletePictureOnClickListener mListenerDelete;
+
+    private SavePictureDialog mDialog;
 
     /**
-     * 是否是只用来查看放大的图片，
+     * 是否是显示多张图片
+     */
+    private boolean isShowManyPicture = false;
+    /**
+     * 是否可以删除图片的图片，
      * true为是，false为否（为true时，显示删除图片的按钮）
      * 默认为false
      */
@@ -67,36 +80,38 @@ public class EnlargePictureDialog extends RxDialog {
         super(context, alpha, gravity);
     }
 
-    public void isCanDeletePicture(boolean isCanDeletePicture) {
-        this.isCanDeletePicture = isCanDeletePicture;
-    }
-
     public void setIsDeleteListener(OnDeletePictureOnClickListener listener) {
-        this.mListener = listener;
+        this.mListenerDelete = listener;
     }
 
     public void setImagePath(String filePath) {
+        this.isShowManyPicture = false;
         initView();
         GlideApp.with(mContext).load(filePath).error(R.mipmap.img_picture_load_failed).into(mImage);
     }
 
     public void setImageUri(Uri uri) {
+        this.isShowManyPicture = false;
         initView();
         GlideApp.with(mContext).load(uri).error(R.mipmap.img_picture_load_failed).into(mImage);
     }
 
 
     public void setImageRes(int resId) {
+        this.isShowManyPicture = false;
         initView();
         mImage.setImageResource(resId);
     }
 
     public void setImageBitmap(Bitmap bitmap) {
+        this.isShowManyPicture = false;
         initView();
         GlideApp.with(mContext).load(bitmap).error(R.mipmap.img_picture_load_failed).into(mImage);
     }
 
-    public void setImageList(List<LocalMedia> images, int position) {
+    public void setImageList(List<String> images, int position, boolean isCanDeletePicture) {
+        this.isShowManyPicture = true;
+        this.isCanDeletePicture = isCanDeletePicture;
         initView();
         initViewPager(images, position);
     }
@@ -109,18 +124,22 @@ public class EnlargePictureDialog extends RxDialog {
         ImageButton mBack = view.findViewById(R.id.ib_left_back);
         mTitle = view.findViewById(R.id.tv_picture_title);
         mDelete = view.findViewById(R.id.ib_delete);
-        mViewPage = view.findViewById(R.id.pv_previewPager);
+        mViewPager = view.findViewById(R.id.pv_previewPager);
         mImage = view.findViewById(R.id.iv_enlarge_picture);
-        if (isCanDeletePicture) {
+        if (isShowManyPicture) {
             mTitle.setVisibility(View.VISIBLE);
-            mDelete.setVisibility(View.VISIBLE);
-            mViewPage.setVisibility(View.VISIBLE);
+            if (isCanDeletePicture) {
+                mDelete.setVisibility(View.VISIBLE);
+            } else {
+                mDelete.setVisibility(View.GONE);
+            }
+            mViewPager.setVisibility(View.VISIBLE);
             mImage.setVisibility(View.GONE);
 
         } else {
             mTitle.setVisibility(View.GONE);
             mDelete.setVisibility(View.GONE);
-            mViewPage.setVisibility(View.GONE);
+            mViewPager.setVisibility(View.GONE);
             mImage.setVisibility(View.VISIBLE);
 
         }
@@ -140,13 +159,13 @@ public class EnlargePictureDialog extends RxDialog {
      * @param images
      * @param position
      */
-    private void initViewPager(final List<LocalMedia> images, int position) {
+    private void initViewPager(final List<String> images, int position) {
         final int[] index = {position};
-        mTitle.setText((position +1) + "/" + images.size());
+        mTitle.setText((position + 1) + "/" + images.size());
         SimpleFragmentAdapter mAdapter = new SimpleFragmentAdapter(images);
-        mViewPage.setAdapter(mAdapter);
-        mViewPage.setCurrentItem(position);
-        mViewPage.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        mViewPager.setAdapter(mAdapter);
+        mViewPager.setCurrentItem(position);
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -154,8 +173,8 @@ public class EnlargePictureDialog extends RxDialog {
 
             @Override
             public void onPageSelected(int position) {
-                index[0] =position;
-                mTitle.setText((position +1)+ "/" + images.size());
+                index[0] = position;
+                mTitle.setText((position + 1) + "/" + images.size());
             }
 
             @Override
@@ -163,6 +182,7 @@ public class EnlargePictureDialog extends RxDialog {
 
             }
         });
+
         mDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -171,7 +191,13 @@ public class EnlargePictureDialog extends RxDialog {
                         .setPositiveButton("确定", new OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                mListener.isDeleteListener(index[0]);
+                                if (mListenerDelete == null) {
+                                    ToastUtil.show(mContext, "删除失败");
+                                    dialog.cancel();
+                                    cancel();
+                                    return;
+                                }
+                                mListenerDelete.isDeleteListener(index[0]);
                                 dialog.cancel();
                                 cancel();
                             }
@@ -187,11 +213,14 @@ public class EnlargePictureDialog extends RxDialog {
         });
     }
 
+    /**
+     * ViewPager适配器类
+     */
     public class SimpleFragmentAdapter extends PagerAdapter {
 
-        private List<LocalMedia> mData = new ArrayList<LocalMedia>();
+        private List<String> mData = new ArrayList<String>();
 
-        public SimpleFragmentAdapter(List<LocalMedia> lists) {
+        public SimpleFragmentAdapter(List<String> lists) {
             mData.clear();
             mData.addAll(lists);
         }
@@ -208,10 +237,20 @@ public class EnlargePictureDialog extends RxDialog {
 
         @NonNull
         @Override
-        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        public Object instantiateItem(@NonNull ViewGroup container, final int position) {
             View view = LayoutInflater.from(mContext).inflate(R.layout.item_enlarge_picture, null);
             ImageView img = view.findViewById(R.id.iv_item_enlarge_picture);
-            GlideApp.with(mContext).load(mData.get(position).getCompressPath()).error(R.mipmap.img_picture_load_failed).into(img);
+            GlideApp.with(mContext).load(mData.get(position)).error(R.mipmap.img_picture_load_failed).into(img);
+
+            if (!isCanDeletePicture) {
+                img.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        savePicture(mData.get(position));
+                        return false;
+                    }
+                });
+            }
             container.addView(view);
             return view;
         }
@@ -222,7 +261,86 @@ public class EnlargePictureDialog extends RxDialog {
         }
     }
 
+    /**
+     * 显示保存图片Dialog
+     */
+    private void savePicture(String url) {
+        if (mDialog == null) {
+            mDialog = new SavePictureDialog(mContext);
+        }
+        mDialog.setUrl(url);
+        mDialog.show();
+    }
+
+    /**
+     * 保存图片Dialog类
+     */
+    public class SavePictureDialog extends RxDialog {
+
+        private String url = null;
+
+        public SavePictureDialog(Context context) {
+            super(context);
+            initViewDialog(context);
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        private void initViewDialog(final Context context) {
+            View view = LayoutInflater.from(context).inflate(R.layout.dialog_save_picture, null);
+            TextView tv_save = view.findViewById(R.id.tv_saveToPhone);
+            TextView tv_cacel = view.findViewById(R.id.tv_cancelToSave);
+            tv_save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (TextUtils.isEmpty(url)) {
+                        ToastUtil.show(context, "图片保存失败");
+                        cancel();
+                        return;
+                    }
+                    BmobFile file = new BmobFile(url.substring(48), "", url);
+                    File saveFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), file.getFilename());
+                    LoggerUtil.e(file.getFilename());
+                    file.download(saveFile, new DownloadFileListener() {
+                        @Override
+                        public void done(String s, BmobException e) {
+                            if (e == null) {
+                                ToastUtil.show(context, "图片已经保存到目录: " + s);
+                                // MediaScanner 扫描更新图库图片
+                                MediaScannerConnection.scanFile(context, new String[]{s}, null, null);
+                            } else {
+                                ToastUtil.show(context, e.getMessage());
+                            }
+                            cancel();
+                        }
+
+                        @Override
+                        public void onProgress(Integer integer, long l) {
+
+                        }
+                    });
+                }
+            });
+
+            tv_cacel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    cancel();
+                }
+            });
+            setContentView(view);
+            mLayoutParams.gravity = 80;
+        }
+    }
+
     public interface OnDeletePictureOnClickListener {
+        /**
+         * 删除图片的位置
+         *
+         * @param position 在List中的序号
+         */
         void isDeleteListener(int position);
     }
 }
